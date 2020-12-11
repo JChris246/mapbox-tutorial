4b50a1441c80dcffec52e2701c775e5477412c03
JChris246 <chris.401@live.com>
2020-12-11 12:24:58 -0400

    feat: Add markers to map
    When user clicks on map, display a popup that prompts the user to save
    the coordinates (lat & lng) with a title. Once saved (to localstorage),
    add the marker to the map at the coordinates

--- src/App.js
+++ src/App.js
@@ -11,41 +11,76 @@ const App = () => {
 
   const [state, setState] = useState({
     coordinates: [13.17080317085379, -59.6034520733074],
-    zoom: 11
+    zoom: 11,
+    selectedCoordinates: null,
+    title: "",
+    map: null,
+    markers: []
   })
 
-
   useEffect(() => {
+    if (!state.map)
+      state.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [state.coordinates[1], state.coordinates[0]],
+        zoom: state.zoom
+      });
 
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [state.coordinates[1], state.coordinates[0]],
-      zoom: state.zoom
-    });
+    state.map.on('click', (e) => {
+      const { lngLat } = e;
 
-    map.on('click', (e) => {
-      console.log(e)
+      setState({...state, "selectedCoordinates": [lngLat.lng, lngLat.lat] });
+      document.getElementById("confirm").style.display = "block";
     })
 
-    map.on('load', () => {
+    state.map.on('load', () => {
       console.log('map has loaded')
 
-      map.addControl(new mapboxgl.NavigationControl(), 'top-left');
-
-      new mapboxgl.Marker({
-        color: "red",
-        // draggable: true
-      }).setLngLat([state.coordinates[1], state.coordinates[0]])
-        .addTo(map);
+      state.map.addControl(new mapboxgl.NavigationControl(), 'top-left');
     })
 
   }, [])
+
+  useEffect(() => {
+    // Save to localstorage whenever markers is updated
+    console.log('save markers to localstorage')
+    window.localStorage.setItem('markers', JSON.stringify(state.markers))
+  }, [state.markers])
+
+  // Update the state object whenever the field is changed
+  const handleFieldChange = (e) => {
+    const { value, name } = e.target
+    
+    setState({...state, [name] : value});
+}
 
   const addMarker = () => {
     //TODO: Add a marker when user clicks on map and display in the left side
     //NOTE: Each marker must have distinct coordinates within Barbados
     //NOTE: Utilize localstorage. No auth required.
+
+    if (state.title.length > 0) {
+      // create a new marker obj with provided coords and title
+      const newMarker = {
+        lng: state.selectedCoordinates[0],
+        lat: state.selectedCoordinates[1],
+        title: state.title
+      }
+
+      // add marker obj to list
+      state.markers = [...state.markers, newMarker]
+
+      // add marker to map
+      new mapboxgl.Marker({
+        color: "red",
+      }).setLngLat([newMarker.lng, newMarker.lat])
+        .addTo(state.map);
+
+      // close popup
+      setState({...state, "title" : ""});
+      document.getElementById("confirm").style.display = "none";
+    }
   }
 
   return (
@@ -58,8 +93,21 @@ const App = () => {
           Marker List
           {/* Add list of todos here */}
         </div>
-        <div id='map' style={{ height: '500px', width: '100%' }}>
+        <div style={{ width: '100%' }}>
+          <div id="confirm" style={{ display: "none" }}>
+            <span>Please enter a title for the marker: </span>
+            <input
+              type="text"
+              placeholder="title"
+              name="title"
+              onChange={handleFieldChange}
+              value={state.title}
+            />
+            <button onClick={addMarker}>Enter</button>
+          </div>
+          <div id='map' style={{ height: '500px'}}>
 
+          </div>
         </div>
       </div>
 
